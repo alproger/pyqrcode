7100af07d1894b7931ba9f42f409799584949007
Kodirov Yakhyokhon <90234997+alproger@users.noreply.github.com>
2021-10-15 23:45:07 +0500

    qrcode

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from qr import get_infoQR       # function to decode qrcode
 
 info = "https://github.com/alproger"
 
-makeQR(info, folder = '/home/alproger/Documents/', bgcolor = 'black', fillcolor = 'white')       # making qrcode for value of info  
+makeQR(info, bgcolor = 'black', fillcolor = 'white')       # making qrcode for value of info  
 
 result = get_infoQR('./examples/5602a3ae-4c3d-42d3-9702-58a005b7271a.png') # decoding and getting info in qrcode
 
